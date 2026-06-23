71fc201ed04274d0653e2d52df02aa5199bb902b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-23 10:54:01 +0200

    Dodanie testów do TestNaming

---
+++ 01_Fundamentals/TestNaming/tests/TestNaming.Tests/AccessControlTests.cs
@@ -0,0 +1,38 @@
+﻿namespace TestNaming.Tests;
+
+public class AccessControlTests
+{
+
+    // {MethodUnderTest}_[Scenario]_[ExpectedResult]
+
+    // {MethodUnderTest}_[Scenario]_[ExpectedBehavior]
+
+    [Fact]
+    public void CanEnter_ActiveBadge_ReturnsTrue()
+    {
+        // Arrange
+        var accessControl = new AccessControl();
+        var badge = new Badge { IsActive = true };
+
+        // Act
+        var result = accessControl.CanEnter(badge);
+
+        // Assert
+        Assert.True(result);
+        
+    }
+
+    [Fact]
+    public void CanEnter_InactiveBadge_ReturnsFalse()
+    {
+        // Arrange
+        var accessControl = new AccessControl();
+        var badge = new Badge { IsActive = false };
+
+        // Act
+        var result = accessControl.CanEnter(badge);
+
+        // Assert
+        Assert.False(result);
+    }
+}
