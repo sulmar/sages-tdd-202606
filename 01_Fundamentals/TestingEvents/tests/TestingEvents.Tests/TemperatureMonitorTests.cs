210cb3b27a22232450574372f5683fbcde544a27
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-24 10:20:39 +0200

    Dodanie przykladu testow zdarzen

--- 01_Fundamentals/TestingEvents/tests/TestingEvents.Tests/TemperatureMonitorTests.cs
+++ 01_Fundamentals/TestingEvents/tests/TestingEvents.Tests/TemperatureMonitorTests.cs
@@ -1,28 +1,60 @@
+using System.Runtime.CompilerServices;
+
 namespace TestingEvents.Tests;
 
 public class TemperatureMonitorTests
 {
+    private readonly TemperatureMonitor monitor = new TemperatureMonitor(25);
+
+    private bool eventRaised = false;
+
+    public TemperatureMonitorTests()
+    {
+        monitor.TemperatureExceeded += (_, _) => eventRaised = true;        
+    }
+
     [Fact]
     public void RecordTemperature_BelowThreshold_DoesNotRaiseEvent()
-    {
-        throw new NotImplementedException();
+    {                
+        // Act
+        monitor.RecordTemperature(20);
+
+        // Assert
+        Assert.False(eventRaised);
     }
 
     [Fact]
     public void RecordTemperature_AtThreshold_DoesNotRaiseEvent()
-    {
-        throw new NotImplementedException();
+    {                
+        // Act
+        monitor.RecordTemperature(25);
+
+        // Assert
+        Assert.False(eventRaised);
     }
 
     [Fact]
     public void RecordTemperature_AboveThreshold_RaisesTemperatureExceededEvent()
     {
-        throw new NotImplementedException();
+        // Act
+        monitor.RecordTemperature(30);
+
+        // Assert
+        Assert.True(eventRaised);
     }
 
     [Fact]
     public void RecordTemperature_AboveThreshold_PassesTemperatureInEventArgs()
     {
-        throw new NotImplementedException();
+        // Arrange         
+        double? recordedTemperature = null;
+
+        monitor.TemperatureExceeded += (_, e) => recordedTemperature = e.Temperature;
+
+        // Act
+        monitor.RecordTemperature(30);
+
+        // Assert
+        Assert.Equal(30, recordedTemperature);
     }
 }
