e0ec8168177f0c6f9a48dec100e20a35893798ff
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-24 10:20:16 +0200

    Dodanie przykladu testow wartosci granicznych

--- 02_TestDesign/BoundaryTesting/TemperatureMonitoring/src/TemperatureMonitoring/TemperatureClassifier.cs
+++ 02_TestDesign/BoundaryTesting/TemperatureMonitoring/src/TemperatureMonitoring/TemperatureClassifier.cs
@@ -2,18 +2,38 @@
 
 public class TemperatureClassifier
 {
-    public string Classify(double temperature)
+    public TemperatureStatus Classify(double temperature) => temperature switch
     {
-        if (temperature < 80)
-        {
-            return "Green";
-        }
+        < 80 => TemperatureStatus.Normal,
+        < 100 => TemperatureStatus.Warning,
+        _ => TemperatureStatus.Critical
+    };
+}
 
-        if (temperature < 100)
-        {
-            return "Orange";
-        }
 
-        return "Red";
-    }
+// Wzorzec projektowy Adapter (za pomoca metody rozszerzacej)
+public static class TemperatureStatusToColorAdapter
+{
+    public static string ToColor(this TemperatureStatus status) => status switch
+    {
+        TemperatureStatus.Normal => ColorIdentifier.Green,
+        TemperatureStatus.Warning => ColorIdentifier.Orange,
+        TemperatureStatus.Critical => ColorIdentifier.Red,
+        _ => throw new NotSupportedException(status.ToString())
+    };
+}
+
+public enum TemperatureStatus
+{
+    Normal,
+    Warning,
+    Critical
+}
+
+
+public static class ColorIdentifier
+{
+    public const string Green = "Green";
+    public const string Orange = "Orange";
+    public const string Red = "Red";
 }
--- 02_TestDesign/BoundaryTesting/TemperatureMonitoring/tests/TemperatureMonitoring.Tests/TemperatureClassifierTests.cs
+++ 02_TestDesign/BoundaryTesting/TemperatureMonitoring/tests/TemperatureMonitoring.Tests/TemperatureClassifierTests.cs
@@ -2,5 +2,44 @@
 
 public class TemperatureClassifierTests
 {
+    [Theory]
+    [InlineData(25, ColorIdentifier.Green)]
+    [InlineData(79, ColorIdentifier.Green)]
+    [InlineData(80, ColorIdentifier.Orange)]
+    [InlineData(99, ColorIdentifier.Orange)]
+    [InlineData(100, ColorIdentifier.Red)]
+    [InlineData(120, ColorIdentifier.Red)]
+    public void Classify_ForGivenTemperature_ShouldReturnExpectedColor(double temperature, string expectedResult)
+    {
+        // Arrange 
+        var classifier = new TemperatureClassifier();        
+
+        // Act 
+        var result = classifier.Classify(temperature);
+
+        // Assert 
+        Assert.Equal(expectedResult, result.ToColor());
+    }
+
+    [Theory]
+    [InlineData(25, TemperatureStatus.Normal)]
+    [InlineData(79, TemperatureStatus.Normal)]
+    [InlineData(80, TemperatureStatus.Warning)]
+    [InlineData(99, TemperatureStatus.Warning)]
+    [InlineData(100, TemperatureStatus.Critical)]
+    [InlineData(120, TemperatureStatus.Critical)]
+    public void Classify_ForGivenTemperature_ShouldReturnExpectedTemperatureStatus(double temperature, TemperatureStatus expectedResult)
+    {
+        // Arrange 
+        var classifier = new TemperatureClassifier();
+
+        // Act 
+        var result = classifier.Classify(temperature);
+
+        // Assert 
+        Assert.Equal(expectedResult, result);
+    }
     
 }
+
+
