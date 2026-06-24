c2605fcb2e91f8dc8b5a043a2ede5bcb93e1a571
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-24 10:18:38 +0200

    Dodanie porownywania obiektow

--- 01_Fundamentals/ObjectEquality/src/ObjectEquality/GpsLocation.cs
+++ 01_Fundamentals/ObjectEquality/src/ObjectEquality/GpsLocation.cs
@@ -1,13 +1,3 @@
 namespace ObjectEquality;
 
-public class GpsLocation
-{
-    public double Latitude { get; }
-    public double Longitude { get; }
-
-    public GpsLocation(double latitude, double longitude)
-    {
-        Latitude = latitude;
-        Longitude = longitude;
-    }
-}
+public record GpsLocation(double Latitude, double Longitude);
