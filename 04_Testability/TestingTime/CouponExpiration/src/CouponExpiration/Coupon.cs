7e31cdd7071c271b21775e941af8b45034c41f90
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-25 13:27:49 +0200

    Update testing time related files

--- 04_Testability/TestingTime/CouponExpiration/src/CouponExpiration/Coupon.cs
+++ 04_Testability/TestingTime/CouponExpiration/src/CouponExpiration/Coupon.cs
@@ -1,16 +1,48 @@
 namespace CouponExpiration;
 
+public interface IClock
+{
+    DateTime UtcNow { get; }
+}
+
+public sealed class SystemClock : IClock
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
+
 public class Coupon
 {
+    private readonly IClock clock;
     public DateTime ExpirationDate { get; }
 
+    private readonly TimeProvider timeProvider;
+
+    //public Coupon(DateTime expirationDate)
+    //    : this(expirationDate, new SystemClock())
+    //{
+
+    //}
+
     public Coupon(DateTime expirationDate)
+        : this(expirationDate, TimeProvider.System)
+    {        
+    }
+
+    public Coupon(DateTime expirationDate, TimeProvider timeProvider)
+    {
+        this.ExpirationDate = expirationDate;
+        this.timeProvider = timeProvider;
+    }
+
+    public Coupon(DateTime expirationDate, IClock now)
     {
         ExpirationDate = expirationDate;
+        this.clock = now;
     }
 
     public bool IsExpired()
     {
-        return DateTime.UtcNow > ExpirationDate;
+        //return clock.UtcNow > ExpirationDate;
+        return timeProvider.GetUtcNow().UtcDateTime > ExpirationDate;
     }
 }
--- 04_Testability/TestingTime/CouponExpiration/tests/CouponExpiration.Tests/CouponTests.cs
+++ 04_Testability/TestingTime/CouponExpiration/tests/CouponExpiration.Tests/CouponTests.cs
@@ -1,28 +1,71 @@
 namespace CouponExpiration.Tests;
 
+internal class FakeClock(DateTime now) : IClock
+{
+    public DateTime UtcNow => now;
+}
+
+public class FakeTimeProvider(DateTime now) : TimeProvider
+{
+    public override DateTimeOffset GetUtcNow() => now;
+}
+
+
 public class CouponTests
 {
     [Fact]
     public void IsExpired_WhenExpirationIsTomorrow_ReturnsFalse()
     {
-        throw new NotImplementedException();
+        // Arrange
+        // var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-26"), now: new FakeClock(DateTime.Parse("2026-06-25")));
+        var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-26"), new FakeTimeProvider(DateTime.Parse("2026-06-25")));
+
+        // Act
+        var result = coupon.IsExpired();
+
+        // Assert
+        Assert.False(result);
     }
 
     [Fact]
     public void IsExpired_WhenExpirationIsYesterday_ReturnsTrue()
     {
-        throw new NotImplementedException();
+        // Arrange
+        // var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-24"), now: new FakeClock(DateTime.Parse("2026-06-25")));
+        var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-24"), new FakeTimeProvider(DateTime.Parse("2026-06-25")));
+
+        // Act
+        var result = coupon.IsExpired();
+
+        // Assert
+        Assert.True(result);
     }
 
     [Fact]
     public void IsExpired_WhenExpirationIsToday_ReturnsFalse()
     {
-        throw new NotImplementedException();
+        // Arrange
+     //   var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-25"), now: new FakeClock(DateTime.Parse("2026-06-25")));
+        var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-25"), new FakeTimeProvider(DateTime.Parse("2026-06-25")));
+
+        // Act
+        var result = coupon.IsExpired();
+
+        // Assert
+        Assert.False(result);
     }
 
     [Fact]
-    public void IsExpired_AtExactExpirationMoment_ReturnsFalse()
+    public void IsExpired_AtExactExpirationFuture_ReturnsFalse()
     {
-        throw new NotImplementedException();
+        // Arrange
+        // var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-30"), now: new FakeClock(DateTime.Parse("2026-06-25")));
+        var coupon = new Coupon(expirationDate: DateTime.Parse("2026-06-30"), new FakeTimeProvider(DateTime.Parse("2026-06-25")));
+
+        // Act
+        var result = coupon.IsExpired();
+
+        // Assert
+        Assert.False(result);
     }
 }
