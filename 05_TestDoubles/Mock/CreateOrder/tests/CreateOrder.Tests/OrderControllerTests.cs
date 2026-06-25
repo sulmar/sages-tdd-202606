69447f7db6ce4f80c849da763f4cce418006160f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-25 13:30:42 +0200

    Testowanie z uzyciem atrapy

--- 05_TestDoubles/Mock/CreateOrder/tests/CreateOrder.Tests/OrderControllerTests.cs
+++ 05_TestDoubles/Mock/CreateOrder/tests/CreateOrder.Tests/OrderControllerTests.cs
@@ -1,10 +1,29 @@
 namespace CreateOrder.Tests;
 
+
+internal class MockMessageClient : IMessageClient
+{
+    public int Count { get; private set; }
+    public void Send(string message)
+    {
+        Count++;
+    }
+}
+
 public class OrderControllerTests
 {
     [Fact]
     public void Post_ValidOrder_SendsMessageOnce()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var mock = new MockMessageClient();
+        var controller = new OrderController(mock);
+        var order = new Order();
+
+        // Act
+        controller.Post(order);
+
+        // Assert
+        Assert.Equal(1, mock.Count);    
     }
 }
